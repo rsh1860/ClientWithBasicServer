df7b4ac8e6c2886b3cf6042842053588be0063f4
rsh1860 <rsh1860@ajou.ac.kr>
2023-01-30 09:29:58 +0900

    Server

---
+++ GameServer/GameServer/Client.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Client
+    {
+        public static int BufferSize = 4096;
+        public int m_id;
+        public TCP m_tcp;
+
+        public Client(int clientId)
+        {
+            m_id = clientId;
+            m_tcp = new TCP(m_id);
+        }
+
+        public class TCP
+        {
+            public TcpClient socket;
+
+            private readonly int id;
+
+            private NetworkStream stream;
+            private byte[] receiveBuffer;
+
+            public TCP(int id)
+            {
+                this.id = id;
+            }
+
+            public void Connect(TcpClient socket)
+            {
+                this.socket= socket;
+                this.socket.ReceiveBufferSize = BufferSize;
+                this.socket.SendBufferSize = BufferSize;
+
+                stream = this.socket.GetStream();
+
+                receiveBuffer = new byte[BufferSize];
+
+                stream.BeginRead(receiveBuffer, 0, BufferSize, ReceiveCallback, null);
+            }
+
+            private void ReceiveCallback(IAsyncResult _result)
+            {
+                try
+                {
+                    int _byteLength = stream.EndRead(_result);
+                    if (_byteLength <= 0)
+                    {
+                        return;
+                    }
+
+                    byte[] _data = new byte[_byteLength];
+                    Array.Copy(receiveBuffer, _data, _byteLength);
+
+                    stream.BeginRead(receiveBuffer, 0, BufferSize, ReceiveCallback, null);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error receiving TCP data : {ex}");
+                }
+            }
+        }
+    }
+}
---
+++ GameServer/GameServer/Server.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Xml.Schema;
+
+namespace GameServer
+{
+    class Server
+    {
+        public static int MaxPlayers { get; set; }
+        public static int Port { get; private set; }
+        public static Dictionary<int, Client> m_clients = new Dictionary<int, Client>();
+        private static TcpListener m_tcpListener;
+
+        public static void Start(int maxPlayers, int port)
+        {
+            MaxPlayers= maxPlayers;
+            Port= port;
+
+            Console.WriteLine("Starting server...");
+
+            InitializeServerData();
+
+            m_tcpListener = new TcpListener(IPAddress.Any, Port);
+            m_tcpListener.Start();
+            m_tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            Console.WriteLine($"Server started on {Port}.");
+        }
+
+        private static void TCPConnectCallback(IAsyncResult result)
+        {
+            TcpClient client = m_tcpListener.EndAcceptTcpClient(result);
+            m_tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+            Console.WriteLine($"Incoming connection from {client.Client.RemoteEndPoint}...");
+
+            for (int i = 0; i <= MaxPlayers; ++i)
+            {
+                if (m_clients[i].m_tcp.socket == null)
+                {
+                    m_clients[i].m_tcp.Connect(client);
+                    return;
+                }
+            }
+
+            Console.WriteLine($"{client.Client.RemoteEndPoint} failed to connect: Server full!");
+        }
+
+        private static void InitializeServerData()
+        {
+            for (int i = 0; i <= MaxPlayers; ++i)
+            {
+                m_clients.Add(i, new Client(i));
+            }
+                
+        }
+    }
+}
